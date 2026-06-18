bca07e4939597783f9d975712e25460674970eb5
Voruganti Abhaya Naga Sai Greeshma <greeshmavoruganti@gmail.com>
2026-06-18 11:46:49 +0530

    Time: 796 ms (5.09%), Space: 47.3 MB (5.99%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -2,27 +2,19 @@ import java.util.*;
 
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-
         int k = s1.length();
-
         char[] p = s1.toCharArray();
         Arrays.sort(p);
         String sort = new String(p);
-
         for(int i = 0; i <= s2.length() - k; i++) {
-
             String sub = s2.substring(i, i + k);
-
             char[] arr = sub.toCharArray();
             Arrays.sort(arr);
-
             String sorted = new String(arr);
-
             if(sorted.equals(sort)) {
                 return true;
             }
         }
-
         return false;
     }
 }
