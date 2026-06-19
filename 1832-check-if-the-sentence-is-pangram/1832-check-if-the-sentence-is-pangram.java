c61652f673231f541db06e7670da83172123743d
Voruganti Abhaya Naga Sai Greeshma <greeshmavoruganti@gmail.com>
2026-06-19 09:24:57 +0530

    Time: 2 ms (62.52%), Space: 42.7 MB (92.5%) - LeetHub

---
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean checkIfPangram(String s) {
+        boolean[] arr=new boolean[26];
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            arr[ch-'a']=true;
+        }
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==false){
+                return false;
+            }}
+            return true;
+}
+}
