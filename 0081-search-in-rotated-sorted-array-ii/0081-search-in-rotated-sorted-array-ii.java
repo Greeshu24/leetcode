f9b9dff398c56122061337089fb759e59970f479
Voruganti Abhaya Naga Sai Greeshma <greeshmavoruganti@gmail.com>
2026-06-24 10:30:21 +0530

    Time: 3 ms (2.91%), Space: 45.2 MB (11.78%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
@@ -50,7 +50,7 @@ class Solution {
 
         while (s <= e) {
 
-            int mid = s + (e - s) / 2;
+            int mid = (s + e)  / 2;
 
             if (nums[mid] == target) {
                 return true;
