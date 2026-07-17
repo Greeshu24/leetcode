cb2f3bf38f8ae297b4ab937e4b46517f01c46c71
Voruganti Abhaya Naga Sai Greeshma <greeshmavoruganti@gmail.com>
2026-07-17 14:28:24 +0530

    Time: 5 ms (70.6%), Space: 43.2 MB (39.97%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.java
+++ 0038-count-and-say/0038-count-and-say.java
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
     public String countAndSay(int n) {
         String s="1";
         for(int k=2;k<=n;k++){
@@ -17,4 +17,30 @@ class Solution {
         s=ans.toString();
 }
 return s;
-}}
+}
+}*/
+class Solution {
+    public String countAndSay(int n) {
+        return solve(1,n,"1");
+    }
+    public String solve(int c,int n,String s){
+        if(c==n)
+        return s;
+        return solve(c+1,n,rle(s));
+    }
+    public String rle(String s){
+                StringBuilder ans=new StringBuilder();
+        int i=0;
+        while(i<s.length()){
+            int cnt=1;
+            while(i+1<s.length() && s.charAt(i)==s.charAt(i+1)){
+                cnt++;
+                i++;
+            }
+            ans.append(cnt);
+            ans.append(s.charAt(i));
+            i++;
+        }
+        return ans.toString();
+    }
+}
